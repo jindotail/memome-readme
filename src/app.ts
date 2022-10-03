058d58ec29a6e135c6479d9c71c389e13501f137
jindo.min <jindo.min@kakaocorp.com>
2022-10-03 23:24:11 +0900

    Feat: 방명록 작성 이미지 파일 api 추가

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "node:path";
+import img from "./assets/images/memome.png";
 
 async function startServer() {
   const app = express();
 
   app.get("/status", (req, res) => {
     res.status(200).end();
+  });
+
+  app.get("/memome", (req, res) => {
+    res.sendFile(path.resolve(__dirname + img));
   });
 
   app.listen(9000, () => {
