163f882b6b810893b23b8fd206b8ae0b2f4e9ee9
jindo.min <jindo.min@kakaocorp.com>
2022-10-04 09:06:43 +0900

    Feat: memome backend로 데이터 요청하기 추가
    - axios

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,34 @@
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
 import express, { Request, Response } from "express";
-import path from "node:path";
-import svg from "./assets/svg/layout.svg";
 import layout from "./view/layout";
+
+interface UserModel {
+  id: string;
+  nickname: string;
+}
+
+interface CommentModel {
+  body: { idx: string; comment: string; iso_time: string }[];
+}
+
+const requestGet = async <T, D>(
+  base: string,
+  url: string,
+  config?: AxiosRequestConfig<D>
+): Promise<T> => {
+  try {
+    const response = await axios.get(new URL(url, base).toString(), config);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      // TODO - custom error 만들기
+      // TODO - 던진 에러 처리해야 함
+      console.log(`Axios Error : ${error.message}`);
+      throw new Error(error.message);
+    }
+    throw error;
+  }
+};
 
 async function startServer() {
   const app = express();
@@ -10,16 +37,26 @@ async function startServer() {
     res.status(200).end();
   });
 
-  app.get("/layout", (req: Request, res: Response) => {
+  app.get("/layout", async (req: Request, res: Response) => {
+    // TODO - http://localhost:8080 환경변수로 변경
+    const user: UserModel = await requestGet(
+      "http://localhost:8080",
+      "/api/user/jindo"
+    );
+    const recentComment: CommentModel = await requestGet(
+      "http://localhost:8080",
+      "/api/comment/jindo"
+    );
+
     res.writeHead(200, {
       "Content-Type": "image/svg+xml",
     });
 
     res.end(
       layout({
-        id: "jindo",
-        nickname: "진도",
-        comment: ["꼬리 다녀감1", "꼬리 다녀감2"],
+        id: user.id,
+        nickname: user.nickname,
+        comment: recentComment.body.map((_) => _.comment).slice(0, 2),
       })
     );
   });
